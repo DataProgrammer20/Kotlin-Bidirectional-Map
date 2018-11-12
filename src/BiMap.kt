a123719544ab836dc76deca8a35785f9261003dd
William <William@BEASTMACHINE>
2018-11-12 01:55:12 -0700

    Adding additional functions for adding and removing items from the map

--- src/BiMap.kt
+++ src/BiMap.kt
@@ -1,6 +1,6 @@
 class BiMap<K, T> {
-    var map: Map<K, T> = emptyMap()
-    var inverseMap: Map<T, K> = emptyMap()
+    private var map: MutableMap<K, T> = mutableMapOf()
+    private var inverseMap: MutableMap<T, K> = mutableMapOf()
     // Empty Constructor
     constructor()
     // Convert arrays to bidirectional map
@@ -9,7 +9,7 @@ class BiMap<K, T> {
         this.inverseMap = arrayMapOfInverse(inverseArray, array)
     }
     // Convert array into map
-    private fun arrayMapOf(array: Array<K>, inverseArray: Array<T>): Map<K, T> {
+    private fun arrayMapOf(array: Array<K>, inverseArray: Array<T>): MutableMap<K, T> {
         val newMap: MutableMap<K, T> = mutableMapOf()
         val pairs = ArrayPair(array, inverseArray, array.size)
         for (itemPair in pairs) {
@@ -18,13 +18,21 @@ class BiMap<K, T> {
         return newMap
     }
     // Convert inverse array into map
-    private fun arrayMapOfInverse(array: Array<T>, inverseArray: Array<K>): Map<T, K> {
+    private fun arrayMapOfInverse(array: Array<T>, inverseArray: Array<K>): MutableMap<T, K> {
         val newMap: MutableMap<T, K> = mutableMapOf()
         val pairs = ArrayPair(array, inverseArray, array.size)
         for (itemPair in pairs) {
             newMap.put(itemPair.first, itemPair.second)
         }
         return newMap
+    }
+    fun add(key: K, value: T) {
+        this.map.put(key, value)
+        this.inverseMap.put(value, key)
+    }
+    fun remove(key: K, value: T) {
+        this.map.remove(key)
+        this.inverseMap.remove(value)
     }
     fun isKeyInMap(key: K): Boolean {return this.map.containsKey(key)}
     fun isValueInMap(value: T): Boolean {return this.inverseMap.containsKey(value)}
